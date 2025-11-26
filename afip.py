afb81450804d137eae17e9b8c1086decbd0fba9f
joaquinvegli <joaquin.vegli@gmail.com>
2025-11-26 09:24:31 -0300

    Update afip.py

--- afip.py
+++ afip.py
@@ -1,67 +1,57 @@
-import os
+import base64
 from pyafipws.wsaa import WSAA
 from pyafipws.wsfev1 import WSFEv1
 
-def init_afip_files():
-    # Carga de certificados desde variables de entorno
-    cert_text = os.getenv("AFIP_CERT_CRT")
-    key_text = os.getenv("AFIP_CERT_KEY")
+class Afip:
+    def __init__(self, cert, key, alias):
+        self.cert = cert
+        self.key = key
+        self.alias = alias
 
-    # Guardar temporalmente los certificados en archivos locales
-    cert_path = "cert-temp.crt"
-    key_path = "key-temp.key"
+    def crear_ticket(self):
+        wsaa = WSAA()
 
-    with open(cert_path, "w") as f:
-        f.write(cert_text)
+        # *** PRODUCCIÓN ***
+        wsaa.HOMO = False
+        wsaa.WSDL = "https://wsaa.afip.gov.ar/ws/services/LoginCms?wsdl"
 
-    with open(key_path, "w") as f:
-        f.write(key_text)
+        ta = wsaa.LoginCMS(
+            self.cert,
+            self.key,
+            "wsfe"
+        )
+        return ta
 
-    return cert_path, key_path
+    def crear_factura(self, ta, data):
+        wsfe = WSFEv1()
 
-def facturar_prueba():
-    cert_path, key_path = init_afip_files()
+        # *** PRODUCCIÓN ***
+        wsfe.Conectar("https://servicios1.afip.gov.ar/wsfev1/service.asmx")
 
-    cuit = int(os.getenv("AFIP_CUIT"))
-    pto_vta = int(os.getenv("AFIP_PTO_VTA", "1"))
-    tipo_cbte = 11  # Factura C
+        wsfe.Cuit = int(data["cuit"])
+        wsfe.Token = ta["token"]
+        wsfe.Sign = ta["sign"]
 
-    # WSAA: autenticación
-    wsaa = WSAA()
-    tra = wsaa.CreateTRA("wsfe")
+        cbte = {
+            "TipoComprobante": data["tipo_cbte"],
+            "PuntoVenta": data["pto_vta"],
+            "NumeroComprobante": data["numero"],
+            "Concepto": data["concepto"],
+            "TipoDocumento": data["tipo_doc"],
+            "NumeroDocumento": data["nro_doc"],
+            "ImporteTotal": data["total"],
+            "ImporteNeto": data["neto"],
+            "ImporteIVA": data["iva"],
+            "FechaComprobante": data["fecha"],
+            "Tributos": [],
+            "Ivas": [
+                {
+                    "Id": data["iva_id"],
+                    "BaseImp": data["neto"],
+                    "Importe": data["iva"]
+                }
+            ]
+        }
 
-    cms = wsaa.SignTRA(tra, cert_path, key_path)
-    ta = wsaa.CallWSAA(cms)
-
-    token = wsaa.ObtenerTagXml(ta, "token")
-    sign = wsaa.ObtenerTagXml(ta, "sign")
-
-    # WSFE: facturación
-    wsfe = WSFEv1()
-    wsfe.Cuit = cuit
-    wsfe.Token = token
-    wsfe.Sign = sign
-
-    # Último comprobante
-    last = wsfe.CompUltimoAutorizado(pto_vta, tipo_cbte)
-    cbte_nro = last[1] + 1
-
-    # Crear factura C de prueba por $1000
-    wsfe.CrearFactura(
-        concepto=1,           # Productos
-        tipo_doc=99,          # Consumidor final
-        nro_doc=0,
-        tipo_cbte=tipo_cbte,
-        punto_vta=pto_vta,
-        cbte_nro=cbte_nro,
-        imp_total=1000.00,
-        imp_neto=1000.00,
-    )
-
-    cae = wsfe.CAESolicitar()
-
-    return {
-        "numero": cbte_nro,
-        "cae": cae
-    }
-
+        resultado = wsfe.CrearFactura(**cbte)
+        return resultado
